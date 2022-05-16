458e5d602c5f6dea9b0421c7dbe28cba6ba39f7b
ayoline <ayo@outlook.com.br>
2022-05-16 18:40:31 -0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Room
+
+# Create your views here.
+
+# rooms = [
+#     {'id':1,'name':'Lets learn Python!'},
+#     {'id':2,'name':'Design with me'},
+#     {'id':3,'name':'Frontend Developers'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
