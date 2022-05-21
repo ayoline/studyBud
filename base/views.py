a45c2a253bacb4fc40e286a2a141126e3b3df3ff
ayoline <ayo@outlook.com.br>
2022-05-20 22:03:56 -0300

    update with new templates

--- base/views.py
+++ base/views.py
@@ -1,6 +1,5 @@
-from multiprocessing import context
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -12,8 +11,10 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q')
+    rooms = Room.objects.filter(topic__name=q)
+    topics = Topic.objects.all()
+    context = {'rooms': rooms,'topics': topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
