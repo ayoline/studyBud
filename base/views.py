7d81c4e2ea65c3c52b048bf32ef7f2bec9f546fb
ayoline <ayo@outlook.com.br>
2022-06-03 20:52:22 -0300

    update the visual to the mobile

--- base/views.py
+++ base/views.py
@@ -193,3 +193,8 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    topics = Topic.objects.filter()
+    return render(request, 'base/topics.html', {'topics':topics})
